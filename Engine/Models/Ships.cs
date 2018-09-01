fab184c350b3bbda8fa6646167f34ab2f0d9136e
Jason <jason.rh.morgan@outlook.com>
2018-08-31 22:21:30 -0400

    Add Code for Ships

--- Engine/Models/Ships.cs
+++ Engine/Models/Ships.cs
@@ -8,5 +8,17 @@ namespace Engine.models
 {
     class Ships
     {
+        public string ShipName { get; set; }
+        
+        public void Ship()
+        {
+
+            var ship1 = new Ships();
+            ship1.ShipName = "Enterprise";
+
+            var ship2 = new Ships();
+            ship2.ShipName = "Defiant";
+
+        }
     }
 }
